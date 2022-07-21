50b2d51f01efe7d9c12ca8bf2891d7523534f4c8
rushi1222 <k.rushi1222@gmail.com>
2022-07-21 16:03:31 +0530

    v81

--- routes/admin.js
+++ routes/admin.js
@@ -15,10 +15,9 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-products.push({title: req.body.title})
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
 exports.routes = router;
 exports.products = products;
-
--- routes/shop.js
+++ routes/shop.js
@@ -3,14 +3,12 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-const adminData = require('./admin');
-
 router.get('/', (req, res, next) => {
-  console.log('shop.js',adminData.products)
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = router;
