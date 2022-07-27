f29e2c2d2d920f2b3fc5f68cb333207888311aeb
rushi1222 <k.rushi1222@gmail.com>
2022-07-27 12:44:55 +0530

    v104

--- controllers/products-demo.js
+++ controllers/products-demo.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product-demo');
 
-const getAddProduct = (req,res,next)=>{
+ exports.getAddProduct = (req,res,next)=>{
     res.render('add-product',{
         pageTitle: 'Add Product',
         path: '/admin/add-product',
@@ -17,14 +17,16 @@ exports.posstAddProduct = (req,res,next)=>{
 };
 
 exports.getProducts = (req,res,next)=>{
-    const products = Product.fetchAll();
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    Product.fetchAll(products=>{
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+    
+        } )
+    });
 
-    } )
 }
--- models/product-demo.js
+++ models/product-demo.js
@@ -1,13 +1,38 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = cb =>{
+    fs.readFile(p,(err,fileContent)= cb=>{
+        if(err){
+            cb([]);
+        }
+        else{
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
 
 module.exports = class Product {
-    construtor(t){
+    constructor(t){
         this.title = t;
     }
     save() {
-        products.push(this);
+        getProductsFromFile(products=>{
+            products.push(this);
+            fs.writeFile(p,JSON.stringify(products),err =>{
+                console.log(err);
+            });
+        });
     }
-    static fetchAll(){
-        return products;
-    }
+
+        static fetchAll(cb){
+            getProductsFromFile(cb)
+        }
+    
 }
